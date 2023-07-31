7c718cb26603c8dab884084ca77f78a87eeb36cc
TrinketBen <ben@trinketstudios.com>
2023-07-31 11:41:00 -0500

    Add some logging to diagnose

--- Editor/ScriptRunner/ScriptRunner.cs
+++ Editor/ScriptRunner/ScriptRunner.cs
@@ -1,8 +1,11 @@
 ﻿using SuperUnityBuild.BuildTool;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using UnityEditor;
+using UnityEngine;
 
 namespace SuperUnityBuild.BuildActions
 {
@@ -10,7 +13,14 @@ namespace SuperUnityBuild.BuildActions
     {
         [BuildTool.FilePath(false, true, "Select program/script to run.")]
         public string scriptPath = "";
+
         public string scriptArguments = "";
+
+        string[] _keychains;
+        int _selectedKeychainIndex;
+        GUIContent[] _guiContent;
+        int[] _options;
+
 
         public override void Execute()
         {
@@ -27,14 +37,57 @@ namespace SuperUnityBuild.BuildActions
 
         private void RunScript(string scriptPath, string arguments)
         {
+            if (string.IsNullOrEmpty(scriptPath))
+            {
+                UnityEngine.Debug.LogError("Empty script path!");
+                return;
+            }
+
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = Path.GetFullPath(scriptPath);
 
             if (!string.IsNullOrEmpty(arguments))
                 startInfo.Arguments = arguments;
 
+            UnityEngine.Debug.Log($"About to start process {scriptPath} with arguments: {arguments}");
             Process proc = Process.Start(startInfo);
             proc.WaitForExit();
+        }
+
+        protected override void DrawProperties(SerializedObject obj)
+        {
+            base.DrawProperties(obj);
+
+            if (_keychains == null)
+            {
+                _keychains = BuildSettings.projectConfigurations.BuildAllKeychains();
+                _guiContent = _keychains.Select(bc => new GUIContent(bc)).ToArray();
+                _options = Enumerable.Range(0, _keychains.Length).ToArray();
+            }
+
+            EditorGUILayout.BeginHorizontal();
+            
+            _selectedKeychainIndex = EditorGUILayout.IntPopup(_selectedKeychainIndex, _guiContent, _options);
+
+            GUI.enabled = _selectedKeychainIndex >= 0;
+            if (GUILayout.Button("Test", GUILayout.ExpandWidth(true)))
+            {
+                BuildReleaseType releaseType;
+                BuildPlatform platform;
+                BuildArchitecture architecture;
+                BuildDistribution distribution;
+                BuildScriptingBackend scriptingBackend;
+                string configKey = _keychains[_selectedKeychainIndex];
+
+                BuildSettings.projectConfigurations.ParseKeychain(configKey, out releaseType, out platform, out architecture, out scriptingBackend, out distribution);
+
+                string resolvedScriptPath = BuildProject.ResolvePath(scriptPath, releaseType, platform, architecture, scriptingBackend, distribution, DateTime.Now);
+                string resolvedScriptArgs = BuildProject.ResolvePath(scriptArguments, releaseType, platform, architecture, scriptingBackend, distribution, DateTime.Now);
+                RunScript(resolvedScriptPath, resolvedScriptArgs);
+
+            }
+            
+            EditorGUILayout.EndHorizontal();
         }
     }
 }
